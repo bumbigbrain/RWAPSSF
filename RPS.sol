03f538d3c0d4546b4f200bd38c36bd8942a4c7af
wen <owen0813715430@hotmail.com>
2024-02-13 23:27:08 +0700

    add timeout system

--- RPS.sol
+++ RPS.sol
@@ -1,18 +1,24 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.7.0 <0.9.0;
-import "./CommitReveal.sol"
+import "./CommitReveal.sol";
 
 
 contract RPS is CommitReveal{
     struct Player {
         uint choice; // 0 - Rock, 1 - Paper , 2 - Scissors, 3 - undefined
         address addr;
+    
     }
+
+    uint256 public constant TIMEOUT = 1 hours;
+    
     uint public numPlayer = 0;
     uint public reward = 0;
     mapping (uint => Player) public player;
     uint public numInput = 0;
+    uint public numReveal = 0;
+    uint public last_act_timestamp = 0;
 
     function addPlayer() public payable {
         require(numPlayer < 2);
@@ -21,19 +27,35 @@ contract RPS is CommitReveal{
         player[numPlayer].addr = msg.sender;
         player[numPlayer].choice = 3;
         numPlayer++;
+        last_act_timestamp = block.timestamp;
     }
 
-    function commit_input(bytes32 hashed_choice, uint idx) public  {
+    function getHash(uint choice, uint salt) public view returns (bytes32) {
+        return getSaltedHash(bytes32(choice), bytes32(salt));
+    }
+
+    function commitInput(bytes32 hashed_choice, uint idx) public  {
         require(numPlayer == 2);
         require(msg.sender == player[idx].addr);
         
-        commit(hashed_choice)
+        commit(hashed_choice);
 
         numInput++;
-        if (numInput == 2) {
+        last_act_timestamp = block.timestamp;
+    }
+
+    function revealInput(uint choice, uint salt, uint idx) public {
+        require(msg.sender == player[idx].addr);
+        require(numInput == 2); 
+        revealAnswer(bytes32(choice), bytes32(salt));
+        numReveal++;
+        if (numReveal == 2){
             _checkWinnerAndPay();
         }
+        last_act_timestamp = block.timestamp;
     }
+
+
 
     function _checkWinnerAndPay() private {
         uint p0Choice = player[0].choice;
@@ -53,5 +75,66 @@ contract RPS is CommitReveal{
             account0.transfer(reward / 2);
             account1.transfer(reward / 2);
         }
+
+        _reset();
     }
+    
+    function isTimeout() public {
+        require(msg.sender == player[0].addr || msg.sender == player[1].addr);
+        require(block.timestamp > last_act_timestamp + TIMEOUT);
+        require(numPlayer > 0);
+
+
+        address payable account0 = payable(player[0].addr);
+        address payable account1 = payable(player[1].addr);
+        
+        // not enough player to play -> return to the player who joined the game 
+        if (numPlayer < 2) {
+            account0.transfer(reward);
+            _reset();
+            return;
+        }
+
+        
+        
+        // player not commit in time or all player commit but not reveal -> return 1 ether for each
+        if (numInput < 2 || numReveal == 0) {
+            account0.transfer(1 ether);
+            account1.transfer(1 ether);
+            _reset();
+            return;
+        }
+
+        // player not reveal in time
+        if (commits[account0].revealed && !commits[account1].revealed) {
+            account0.transfer(reward);
+            _reset();
+            return;
+        }
+
+        if (commits[account1].revealed && !commits[account0].revealed) {
+            account1.transfer(reward);
+            _reset();
+            return;
+        }
+        
+        
+
+
+    }
+    
+    function _reset() private {
+        numPlayer = 0;
+        reward = 0;
+        numInput = 0;
+        numReveal = 0;
+        last_act_timestamp = 0;
+        delete commits[player[0].addr];
+        delete commits[player[1].addr];
+        delete player[0];
+        delete player[1];
+
+    }
+
+    
 }
